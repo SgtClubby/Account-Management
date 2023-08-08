6754b546eeb1fce54be43811e60618db44ef67de
Clomby <clomby@metrix.pw>
2023-08-08 08:42:14 +0200

    global types file

---
+++ src/app/types.ts
@@ -0,0 +1,26 @@
+import { Session } from "next-auth";
+
+export type Account = {
+  id: string;
+  name: string;
+  username: string;
+  password: string;
+  usedFor: string;
+};
+
+export type User = {
+  _id: string;
+  username: string;
+  email: string;
+  password: string;
+  twoFactorAuth: Boolean;
+};
+
+export interface SessionWithId extends Session {
+  user?: {
+    id?: string;
+    name?: string | null;
+    email?: string | null;
+    image?: string | null;
+  };
+}
