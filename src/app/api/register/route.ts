1aae78d3347cfea3c3cc35e1fb8fe4f39a3ba37b
Clomby <clomby@metrix.pw>
2023-08-08 08:42:27 +0200

    register api endpoint

---
+++ src/app/api/register/route.ts
@@ -0,0 +1,58 @@
+import { NextResponse } from "next/server";
+import { User } from "../../../../mongo/mongo";
+import { hash } from "bcrypt";
+
+export async function POST(request: Request, response: Response) {
+  const body = await request.json();
+
+  const email = body.email;
+  const un = body.username;
+  const pw = body.password;
+
+  if (!un || !pw || !email) {
+    return NextResponse.json(
+      { message: "Please fill all fields.", ok: false, status: 400 },
+      { status: 400 }
+    );
+  }
+
+  const usernameCheck = await User.findOne({ username: un });
+  if (usernameCheck) {
+    return NextResponse.json(
+      { message: "Username already in use.", ok: false, status: 400 },
+      { status: 400 }
+    );
+  }
+
+  const emailCheck = await User.findOne({ email });
+  if (emailCheck) {
+    return NextResponse.json(
+      { message: "Email already in use.", ok: false, status: 400 },
+      { status: 400 }
+    );
+  }
+
+  const newUser = new User({
+    username: un as string,
+    email: email as string,
+    password: await hash(pw, 10),
+    twoFactorAuth: false,
+    twoFactorAuthSecret: null,
+  });
+
+  return newUser
+    .save()
+    .then(() => {
+      return NextResponse.json(
+        {
+          message: "Success! You will be forwarded soon!",
+          ok: true,
+          status: 200,
+        },
+        { status: 200 }
+      );
+    })
+    .catch((e: any) => {
+      return console.log(e);
+    });
+}
