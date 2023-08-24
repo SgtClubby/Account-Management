a8ba40fa5d31699c8015599accc7629c788a5ece
Clomby <clomby@metrix.pw>
2023-08-24 10:33:22 +0200

    Major refactor

--- src/app/accounts/page.tsx
+++ src/app/accounts/page.tsx
@@ -1,54 +1,49 @@
 "use client";
 
-import { useSession, signOut } from "next-auth/react";
+import { useSession } from "next-auth/react";
 import { redirect } from "next/navigation";
 import Dropdown from "./Dropdown";
-import { ChangeEvent, useEffect, useState } from "react";
-import { AES, enc } from "crypto-js";
+import { useEffect, useState } from "react";
 import AddModal from "./AddModal";
-import TwoFactorModal from "./TwoFactorModal";
 import SyncIcon from "@mui/icons-material/Sync";
 import AddBoxIcon from "@mui/icons-material/AddBox";
-import { Account, SessionWithId } from "../types";
+import { Account, User } from "../types";
 import AccountCard from "./AccountCard";
-import PasswordResetModal from "./ResetPasswordModal";
-import { decrypt } from "lib/functions";
+import useAlert from "hooks/useAlert";
+import useForm from "hooks/useForm";
+import useRefresh from "hooks/useRefresh";
 
 export default function Home() {
   const { data: session, status } = useSession();
 
-  if (status === "unauthenticated") {
-    redirect("/login");
-  }
+  useEffect(() => {
+    if (status == "unauthenticated") {
+      redirect("/login");
+    }
+  }, [session, status]);
+
+  const { refresh, refreshState } = useRefresh();
 
   const [accounts, setAccounts] = useState<Account[]>();
-  const [accountsCache, setAccountsCache] = useState<Account[]>();
-  const [search, setSearch] = useState<string>("");
+  const [emailVerified, setEmailVerified] = useState<any>(undefined);
 
-  async function fetchData() {
-    setAccountsCache(undefined);
-    const res = await fetch("/api/accounts", {
-      method: "GET",
-    });
-    const data = await res.json();
-
-    setAccountsCache(data.accounts);
-    setAccounts(data.accounts);
-  }
-
-  const [emailVerified, setEmailVerified] = useState<string | null>(null);
-
-  async function fetchChecks() {
-    const res = await fetch("/api/checks", {
-      method: "GET",
-    });
-    const data = await res.json();
-    if (!data?.user?.email?.emailVerified) {
-      setEmailVerified(data?.user?.email?.details?.message);
-    } else {
-      setEmailVerified(null);
+  useEffect(() => {
+    if (session) {
+      setAccounts(undefined);
+      Promise.all([fetch("/api/accounts"), fetch("/api/me")]).then(
+        ([accountsRes, meRes]) => {
+          accountsRes.json().then((data) => {
+            setAccounts(data.accounts);
+          });
+          meRes.json().then((data) => {
+            if (!data.user.emailVerified) setEmailVerified(true);
+          });
+        }
+      );
     }
-  }
+  }, [session, refresh]);
+
+  const alert = useAlert();
 
   async function resendVerification() {
     const res = await fetch(`/api/verify`, {
@@ -61,72 +56,17 @@ export default function Home() {
         id: session?.user?.id,
       }),
     });
-
     const data = await res.json();
-
     if (data.ok) {
-      setError({
-        show: true,
-        message: "Email verification sent!",
-      });
-      setTimeout(() => {
-        setError({ show: false, message: "" });
-      }, 10000);
+      alert.showAlertForSeconds("Verification email sent!", 5);
     } else {
-      setError({
-        show: true,
-        message: data.message,
-      });
-      setTimeout(() => {
-        setError({ show: false, message: "" });
-      }, 10000);
+      alert.showAlertForSeconds(data.message, 5);
     }
-
-    let interval = setInterval(async () => {
-      fetchChecks();
-
-      if (!emailVerified) {
-        clearInterval(interval);
-      }
-    }, 10000);
   }
 
-  useEffect(() => {
-    fetchChecks();
-  }, []);
-
-  const [formValues, setFormValues] = useState({
-    masterPassword: "",
-    decrypted: false,
-  });
-
-  const handleChange = (event: ChangeEvent<HTMLInputElement>) => {
-    const { name, value } = event.target;
-    setFormValues({ ...formValues, [name]: value });
-  };
-
-  useEffect(() => {
-    if (session) fetchData();
-  }, [session]);
-
-  useEffect(() => {
-    if (search) {
-      setAccounts(accountsCache);
-      const filteredAccounts = accountsCache?.filter((account) => {
-        return account.name.toLowerCase().includes(search.toLowerCase());
-      });
-      setAccounts(filteredAccounts);
-    }
-  }, [search]);
-
   const [addModal, showAddModal] = useState({ show: false });
-  const [passwordResetModal, showPasswordResetModal] = useState({
-    show: false,
-  });
-  const [twoFactorModal, showTwoFactorModal] = useState({ show: false });
-  const [error, setError] = useState({ show: false, message: "" });
 
-  const [syncing, setSyncing] = useState<NodeJS.Timeout | Boolean>(false);
+  const [search, setSearch] = useState("");
 
   return (
     <section className="py-28">
@@ -134,26 +74,19 @@ export default function Home() {
         addModal={addModal}
         showAddModal={showAddModal}
         session={session}
-        fetchData={fetchData}
       />
-      <TwoFactorModal
-        twoFactorModal={twoFactorModal}
-        showTwoFactorModal={showTwoFactorModal}
-        session={session as SessionWithId}
-      />
-      <PasswordResetModal
-        passwordResetModal={passwordResetModal}
-        showPasswordResetModal={showPasswordResetModal}
-        session={session as SessionWithId}
-      />
-      {error.show && (
+      {alert.data.show && (
         <div className="fixed z-[100] bg-green-100 border border-green-400 text-greeb-700 px-4 py-3 rounded w-fit max-w-64 right-0 left-0 m-auto">
-          <span className="block sm:inline">{error.message}</span>
+          <span className="block sm:inline">{alert.data.message}</span>
         </div>
       )}
+
       {emailVerified && (
         <div className="fixed flex flex-col z-[100] bg-red-100 border border-red-400 text-sm md:text-md text-gray-800 px-4 py-3 rounded w-80 md:w-fit right-0 left-0 top-5 m-auto">
-          <span className="block sm:inline">{emailVerified}</span>
+          <span className="block sm:inline">
+            You need to verify your email, please check your inbox or spam
+            folder!
+          </span>
           <button
             className="text-blue-500 hover:text-blue-600"
             onClick={() => resendVerification()}
@@ -162,6 +95,7 @@ export default function Home() {
           </button>
         </div>
       )}
+
       {/* <button
         className="fixed z-[100] bottom-5 right-5 bg-blue-500 hover:bg-blue-600 text-white font-bold py-2 px-4 rounded"
         onClick={() => {
@@ -192,15 +126,12 @@ export default function Home() {
             <SyncIcon
               fontSize="large"
               className="text-gray-200 ml-1 mt-[2px] hover:text-gray-400 cursor-pointer"
-              onClick={() => fetchData()}
+              onClick={refreshState}
               titleAccess="Refresh list"
             />
 
             <div className="ml-auto items-center">
-              <Dropdown
-                showTwoFactorModal={showTwoFactorModal}
-                showPasswordResetModal={showPasswordResetModal}
-              />
+              <Dropdown />
             </div>
           </div>
         </div>
@@ -240,17 +171,7 @@ export default function Home() {
               accounts?.map((account, idx) => (
                 <>
                   <div className="w-full h-px bg-gray-700 mb-3"></div>
-                  <AccountCard
-                    key={idx + account.password}
-                    idx={idx}
-                    setError={setError}
-                    account={account}
-                    decrypt={decrypt}
-                    handleChange={handleChange}
-                    formValues={formValues}
-                    setFormValues={setFormValues}
-                    fetchData={fetchData}
-                  />
+                  <AccountCard key={idx} idx={idx} account={account} />
                 </>
               ))}
           </div>
