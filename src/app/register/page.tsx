b7b2b632dc51693e7e41400bcf3a09c92d6b9054
Clomby <clomby@metrix.pw>
2023-08-24 10:38:49 +0200

    Removed the check for password as its buggy, any password is accepted, and not validated for now. TODO: refactor password validation logic to properly validate passwords.

--- src/app/register/page.tsx
+++ src/app/register/page.tsx
@@ -33,8 +33,7 @@ export default function LoginForm() {
   const [satisfiesDI, setsatisfiesDI] = useState(false);
   const [satisfiesSC, setsatisfiesSC] = useState(false);
 
-  const [satisfiesAll, setsatisfiesAll] = useState(false);
-
+  const [satisfiesAll, setsatisfiesAll] = useState(true);
   const callbackUrl = "/accounts";
 
   useEffect(() => {
@@ -65,27 +64,27 @@ export default function LoginForm() {
     ) {
       setsatisfiesAll(true);
     } else {
-      setsatisfiesAll(false);
+      setsatisfiesAll(true);
     }
 
     validatePassword(formValues.password);
   }, [formValues.password]);
 
-  const [valid, setValid] = useState(false);
+  const [matchingPassword, setMatchingPassword] = useState(false);
   useEffect(() => {
     if (!formValues.tempCheckPassword || !formValues.password) {
       setMatch(null);
-      setValid(false);
+      return setMatchingPassword(false);
     }
 
     if (formValues.password !== formValues.tempCheckPassword) {
       setMatch("Passwords do not match!");
-      setValid(false);
+      return setMatchingPassword(false);
     } else {
       setMatch(null);
-      setValid(true);
+      return setMatchingPassword(true);
     }
-  }, [formValues.tempCheckPassword === formValues.password]);
+  }, [formValues.tempCheckPassword, formValues.password]);
 
   const onSubmit = async (e: React.FormEvent) => {
     setError("");
@@ -153,7 +152,7 @@ export default function LoginForm() {
 
   return (
     <div className="fixed left-[50%] top-[50%] translate-x-[-50%] translate-y-[-50%]">
-      <h1 className="text-gray-200 text-2xl text-center mb-10 font-extrabold sm:text-3xl">
+      <h1 className="text-gray-200 mt-10 text-2xl text-center mb-10 font-extrabold mt:mt-0 md:text-4xl w-72 md:w-96">
         Register new account
       </h1>
       <form className="w-72 md:w-96" onSubmit={onSubmit}>
@@ -193,10 +192,53 @@ export default function LoginForm() {
             value={formValues.password}
             onChange={handleChange}
             placeholder="Password"
-            className={`${input_style}`}
+            className={`${input_style} absolute w-72 md:w-96`}
           />
+          <div
+            className={`${
+              formValues.password.length == 0
+                ? "hidden relative mb-6"
+                : "relative top-[70px] lg:top-[0px] lg:left-[400px] bg-white border-gray-200 border-2 rounded p-2 text-sm text-gray-700 w-fit py-2 px-4 z-10 shadow-md"
+            }`}
+          >
+            <p className="text-gray-900 mb-2">Password recommendation:</p>
+            <ul className="list-disc list-inside">
+              <li
+                className={`${satisfiesLG ? "text-green-500" : "text-red-500"}`}
+              >
+                At least 8 characters
+              </li>
+              <li
+                className={`${satisfiesLC ? "text-green-500" : "text-red-500"}`}
+              >
+                At least one lower case letter
+              </li>
+              <li
+                className={`${satisfiesUP ? "text-green-500" : "text-red-500"}`}
+              >
+                At least one upper case letter
+              </li>
+              <li
+                className={`${satisfiesDI ? "text-green-500" : "text-red-500"}`}
+              >
+                At least one digit
+              </li>
+              <li
+                className={`${satisfiesSC ? "text-green-500" : "text-red-500"}`}
+              >
+                At least one special character
+              </li>
+            </ul>
+          </div>
         </div>
-        <div className={`${formValues.password.length == 0 ? "mb-6" : "mb-2"}`}>
+        <div className="mb-20 lg:mb-0"></div>
+        <div
+          className={
+            formValues.password.length == 0
+              ? "mt-28 mb-6"
+              : "absolute lg:mt-[-85px] w-full"
+          }
+        >
           <input
             required
             type="password"
@@ -207,65 +249,37 @@ export default function LoginForm() {
             className={`${input_style}`}
           />
         </div>
-        {match && <p className="text-red-500 mb-6">{match}</p>}
-        {/* Make a password requirenent list using the states*/}
         <div
-          className={`${
-            formValues.password.length == 0 ? "hidden mb-6" : "block mb-6"
-          }`}
+          className={
+            formValues.password.length == 0
+              ? ""
+              : "lg:mt-0 mt-40 small:grid small:grid-cols-[1fr_0.2fr_1fr] small:grid-rows-2"
+          }
         >
-          <p className="text-gray-200 mb-2">Password requirements:</p>
-          <ul className="list-disc list-inside">
-            <li
-              className={`${satisfiesLG ? "text-green-500" : "text-red-500"}`}
-            >
-              At least 8 characters
-            </li>
-            <li
-              className={`${satisfiesLC ? "text-green-500" : "text-red-500"}`}
-            >
-              At least one lower case letter
-            </li>
-            <li
-              className={`${satisfiesUP ? "text-green-500" : "text-red-500"}`}
-            >
-              At least one upper case letter
-            </li>
-            <li
-              className={`${satisfiesDI ? "text-green-500" : "text-red-500"}`}
-            >
-              At least one digit
-            </li>
-            <li
-              className={`${satisfiesSC ? "text-green-500" : "text-red-500"}`}
-            >
-              At least one special character
-            </li>
-          </ul>
+          {match && <p className="text-red-500 mb-6 col-span-3">{match}</p>}
+          <button
+            type="submit"
+            className={classNames(
+              `inline-block px-7 py-4 bg-metrix-blue text-white font-medium text-sm leading-snug uppercase rounded shadow-md hover:bg-metrix-blue-hover hover:shadow-lg focus:bg-metrix-blue-hover focus:shadow-lg focus:outline-none focus:ring-0 active:bg-blue-800 active:shadow-lg transition duration-150 ease-in-out w-full`
+            )}
+            disabled={loading}
+          >
+            {loading ? "loading..." : "Register"}
+          </button>
+          <div className="flex items-center my-4 before:flex-1 before:border-t before:border-gray-300 before:mt-0.5 after:flex-1 after:border-t after:border-gray-300 after:mt-0.5">
+            <p className="text-center text-gray-200 font-semibold mx-4 mb-0">
+              OR
+            </p>
+          </div>
+          <button
+            onClick={() => router.push("/login")}
+            disabled={loading}
+            className="inline-block px-7 py-4 bg-white text-gray-900 text-center font-medium text-sm leading-snug uppercase rounded shadow-md hover:bg-metrix-blue-hover hover:shadow-lg focus:bg-metrix-blue-hover focus:shadow-lg focus:outline-none focus:ring-0 active:bg-blue-800 active:shadow-lg transition duration-150 ease-in-out w-full"
+          >
+            Back
+          </button>
         </div>
-        <button
-          type="submit"
-          className={classNames(
-            `inline-block px-7 py-4 bg-metrix-blue text-white font-medium text-sm leading-snug uppercase rounded shadow-md hover:bg-metrix-blue-hover hover:shadow-lg focus:bg-metrix-blue-hover focus:shadow-lg focus:outline-none focus:ring-0 active:bg-blue-800 active:shadow-lg transition duration-150 ease-in-out w-full`,
-            !valid || !satisfiesAll ? "cursor-not-allowed" : "cursor-pointer"
-          )}
-          disabled={loading || !valid || !satisfiesAll}
-        >
-          {loading ? "loading..." : "Register"}
-        </button>
       </form>
-      <div className="flex items-center my-4 before:flex-1 before:border-t before:border-gray-300 before:mt-0.5 after:flex-1 after:border-t after:border-gray-300 after:mt-0.5">
-        <p className="text-center text-gray-200 font-semibold mx-4 mb-0">OR</p>
-      </div>
-      <div className="flex items-center">
-        <button
-          onClick={() => router.push("/login")}
-          disabled={loading}
-          className="inline-block px-7 py-4 bg-metrix-blue text-white text-center font-medium text-sm leading-snug uppercase rounded shadow-md hover:bg-metrix-blue-hover hover:shadow-lg focus:bg-metrix-blue-hover focus:shadow-lg focus:outline-none focus:ring-0 active:bg-blue-800 active:shadow-lg transition duration-150 ease-in-out w-full"
-        >
-          Back
-        </button>
-      </div>
     </div>
   );
 }
