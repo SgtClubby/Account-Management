fa2595dd16551377c469a2865d59b219cf53f432
Clomby <clomby@metrix.pw>
2023-08-24 10:34:19 +0200

    Added option to remove 2fa

--- src/app/api/2fa/remove2fa/route.ts
+++ src/app/api/2fa/remove2fa/route.ts
@@ -2,9 +2,11 @@ import { NextResponse } from "next/server";
 import { getServerSession } from "next-auth";
 import { authOptions } from "pages/api/auth/[...nextauth]";
 import { User } from "mongo/mongo";
+import speakeasy from "speakeasy";
 import { SessionWithId, User as UserType } from "@/app/types";
+import { decrypt } from "lib/functions";
 
-export async function GET(request: Request, response: Response) {
+export async function DELETE(request: Request, response: Response) {
   const session = (await getServerSession(authOptions)) as SessionWithId;
   if (!session) {
     return NextResponse.json(
@@ -12,8 +14,36 @@ export async function GET(request: Request, response: Response) {
       { status: 401 }
     );
   }
+  const body = await request.json();
+  const token = body.token;
 
-  User.findByIdAndUpdate(
+  const salt = (await User.findById(session?.user?.id))?.salt;
+  const authSecret = (await User.findById(session?.user?.id))
+    ?.twoFactorAuthSecret;
+
+  if (!salt || !authSecret) {
+    return NextResponse.json(
+      { message: "You have to be logged in for this..." },
+      { status: 401 }
+    );
+  }
+
+  const secret = decrypt(authSecret, salt + process.env.PEPPER);
+
+  const verified = speakeasy.totp.verify({
+    secret: secret,
+    encoding: "base32",
+    token: token,
+  });
+
+  if (!verified) {
+    return NextResponse.json(
+      { message: "Invalid token", ok: false },
+      { status: 401 }
+    );
+  }
+
+  await User.findByIdAndUpdate(
     { _id: session?.user?.id },
     {
       twoFactorAuth: false,
