8f591a8fd194407a62505d8255a00712b332cf6b
Clomby <clomby@metrix.pw>
2023-08-24 10:23:26 +0200

    Removed env

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -5,9 +5,6 @@ module.exports = {
       script: "npm run start",
       out_file: "/var/log/pm2/acc-man/log.log",
       error_file: "/var/log/pm2/acc-man/error.log",
-      env: {
-        ENV: "prod",
-      },
     },
   ],
 };
