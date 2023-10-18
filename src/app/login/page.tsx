b745fd8ccf557911ae7db1d43bca7fb8a2d7f9ca
Clomby <clomby@metrix.pw>
2023-10-18 11:37:30 +0200

    Added open redirect, fixed some typos, added new alert system

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -1,72 +1,70 @@
 "use client";
 
-import { logger } from "lib/logger";
+import useAlert from "hooks/useAlert";
+import useForm from "hooks/useForm";
 import { signIn, useSession } from "next-auth/react";
-import Image from "next/image";
 import {
   useSearchParams,
   useRouter,
   ReadonlyURLSearchParams,
 } from "next/navigation";
-import { ChangeEvent, useState } from "react";
+import { useEffect, useState } from "react";
 
 export default function LoginForm() {
   const { data: session, status } = useSession();
 
+  // open redirect
+  const searchParams = useSearchParams() as ReadonlyURLSearchParams;
+  let redirectUrl = searchParams.get("redirect");
+  const openRedirectRegex = new RegExp(/^\/[^\/].*/);
+  const [callbackUrl, setCallbackUrl] = useState("/accounts");
+
+  useEffect(() => {
+    if (redirectUrl && !redirectUrl.match(openRedirectRegex)) {
+      redirectUrl = null;
+    }
+    if (redirectUrl) {
+      setCallbackUrl(redirectUrl);
+    }
+  }, [redirectUrl]);
+
   const router = useRouter();
   if (status === "authenticated") {
-    router.push("/accounts");
+    router.push(callbackUrl);
   }
   const [loading, setLoading] = useState(false);
 
-  const [formValues, setFormValues] = useState({
+  const alert = useAlert();
+
+  const { formValues, setFormValues, handleChange } = useForm({
     username: "",
     password: "",
     code: "",
   });
 
-  const [error, setError] = useState("");
-
-  const searchParams = useSearchParams() as ReadonlyURLSearchParams;
-  const callbackUrl = "/accounts";
-
   const onSubmit = async (e: React.FormEvent) => {
-    setError("");
     e.preventDefault();
+    alert.hideAlert();
     try {
       setLoading(true);
       setFormValues({ username: "", password: "", code: "" });
 
       const res = await signIn("credentials", {
-        redirect: false,
         username: formValues.username,
         password: formValues.password,
         "2fa-key": formValues.code,
-        callbackUrl,
       });
 
       setLoading(false);
 
-      if (!res?.error) {
-        router.push(callbackUrl);
-      } else {
-        setError("Invalid username, password or 2fa code");
+      if (res?.error) {
+        return alert.showAlert("Invalid username, password or 2fa code", false);
       }
     } catch (error: any) {
       setLoading(false);
-      setError(error);
+      alert.showAlert(error.message);
     }
   };
-
-  const handleChange = (event: ChangeEvent<HTMLInputElement>) => {
-    const { name, value } = event.target;
-    setFormValues({ ...formValues, [name]: value });
-  };
-
-  logger(`LoginForm: ${formValues}`, "info");
-
-  const input_style =
-    "mx-auto form-control block px-4 py-3 text-sm w-full font-normal text-gray-700 bg-white bg-clip-padding border border-solid border-gray-300 rounded transition ease-in-out m-0 focus:text-gray-700 focus:bg-white focus:border-metrix-blue focus:outline-none";
 
   return (
     <div className="lg:fixed lg:left-[50%] lg:top-[50%] lg:translate-x-[-50%] lg:translate-y-[-50%] flex flex-col items-center justify-center h-screen w-full">
@@ -83,9 +81,6 @@ export default function LoginForm() {
         </div>
       </div>
       <form autoComplete="off" className="w-72 md:w-96" onSubmit={onSubmit}>
-        {error && (
-          <p className="text-center bg-red-300 py-4 mb-6 rounded">{error}</p>
-        )}
         <div className="mb-6">
           <input
             required
@@ -94,7 +89,7 @@ export default function LoginForm() {
             value={formValues.username}
             onChange={handleChange}
             placeholder="Username"
-            className={`${input_style}`}
+            className="metrix_input"
             autoComplete="username"
           />
         </div>
@@ -106,9 +101,8 @@ export default function LoginForm() {
             value={formValues.password}
             onChange={handleChange}
             placeholder="Password"
-            className={`${input_style}`}
-            autoComplete="off"
-            autoCorrect="off"
+            className="metrix_input"
+            autoComplete="current-password"
           />
           <label>
             <a
@@ -118,6 +112,7 @@ export default function LoginForm() {
               Forgot Password?
             </a>
           </label>
+          <alert.AlertComponent className="block py-2 px-3 mt-3" />
         </div>
         <span>
           <p className="text-gray-200 text-sm font-semibold mb-2">
@@ -131,18 +126,17 @@ export default function LoginForm() {
             value={formValues.code}
             onChange={handleChange}
             placeholder="Two Factor Code"
-            className={`${input_style}`}
+            className="metrix_input"
             autoComplete="off"
             autoCorrect="off"
           />
         </div>
         <button
           type="submit"
-          // style={{ backgroundColor: `${loading ? "#ccc" : "#3446eb"}` }}
           className="inline-block px-7 py-3 bg-metrix-blue text-white font-medium text-sm leading-snug uppercase rounded shadow-md hover:bg-metrix-blue-hover hover:shadow-lg focus:bg-metrix-blue-hover focus:shadow-lg focus:outline-none focus:ring-0 active:bg-blue-800 active:shadow-lg transition duration-150 ease-in-out w-full"
           disabled={loading}
         >
-          {loading ? "loading..." : "Sign In"}
+          {loading ? "Signing in..." : "Sign In"}
         </button>
         <div className="flex items-center my-4 before:flex-1 before:border-t before:border-gray-300 before:mt-0.5 after:flex-1 after:border-t after:border-gray-300 after:mt-0.5">
           <p className="text-center text-gray-200 font-semibold mx-4 mb-0">
